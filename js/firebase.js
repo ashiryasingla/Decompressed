db77715c8f8d4d66a955cb25601c426770c27dea
ashiryasingla <ashiryasingla07@gmail.com>
2026-05-13 14:33:23 -0400

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,23 +1,16 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore, addDoc, collection, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyB2hLSS-5kPYpV_Q8a61SdlXVsWY2Nt_AM",
-    authDomain: "decompressed-ash.firebaseapp.com",
-    projectId: "decompressed-ash",
-    storageBucket: "decompressed-ash.firebasestorage.app",
-    messagingSenderId: "264406149393",
-    appId: "1:264406149393:web:c31a3e0ac87568f51244b0",
-    measurementId: "G-37Z234V2P6"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyB2hLSS-5kPYpV_Q8a61SdlXVsWY2Nt_AM",
+  authDomain: "decompressed-ash.firebaseapp.com",
+  projectId: "decompressed-ash",
+  storageBucket: "decompressed-ash.firebasestorage.app",
+  messagingSenderId: "264406149393",
+  appId: "1:264406149393:web:c31a3e0ac87568f51244b0"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db, addDoc, collection, serverTimestamp };
